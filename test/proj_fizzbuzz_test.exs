34b2b8686af5163f79427c98c50d741210b1e45e
bruno henrique <brunoricksp@gmail.com>
2025-02-24 15:51:35 -0300

    v_publish_fizzbuzz_1.0.1

--- test/proj_fizzbuzz_test.exs
+++ test/proj_fizzbuzz_test.exs
@@ -1,8 +1,11 @@
 defmodule ProjFizzbuzzTest do
   use ExUnit.Case
-  doctest ProjFizzbuzz
 
-  test "greets the world" do
-    assert ProjFizzbuzz.hello() == :world
+  describe "build/1" do
+    test "when file is valid and provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz, 22, :buzz, 29, :fizzbuzz, 32, :fizz, :buzz, :buzz, 44, :buzz, :buzz, 56, 58, 62, :buzz, :fizz, :buzz, :buzz, :fizz, :fizzbuzz, :buzz, :buzz, :fizzbuzz, :buzz]}
+      assert ProjFizzbuzz.build("numbers.txt") == expected_response
+
+    end
   end
 end
